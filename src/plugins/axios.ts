f2041456fdaa21d813d512e50e69edcf89defc91
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2023-02-21 20:05:44 -0300

    Add baseUrl da api de cachorro

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosIns = axios.create({
-    baseURL: 'https://cat-fact.herokuapp.com'
+    baseURL: 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs',
+    // baseURL: 'https://cat-fact.herokuapp.com'
 });
 
 export default axiosIns;
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>Dogs Facts</h1>
+    <div class="container">
+      <div class="row"></div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "@/plugins/axios";
+import CardComponent from "@/components/CardComponent.vue";
+
+export default defineComponent({
+  name: 'AboutView',
+  async mounted(){
+    this.testDogApi();
+  },
+  methods:{
+    async testDogApi(){
+      const dogResponse = await axios.get('all')
+      console.log(dogResponse.data);
+    }
+  },
+});
+</script>
